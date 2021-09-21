ebc0ffde7c2a011b7588acd68a1731e388c804b6
Adamova Galina <adamova@triya.ru>
2021-09-21 15:49:53 +0300

    Доработанная версия

--- Decorator_func.py
+++ Decorator_func.py
@@ -1,17 +1,18 @@
 from datetime import datetime
 import hashlib
+import requests
 
 path = 'Log.txt'
 
-#задание №2
+#задание №3
 def my_decorator(old_function):
-    def decor(get_path):
+    def decor(get_path_vk):
         def new_function(*args, **kwargs):
-            f = open('Log.txt', 'a', encoding='utf8')
+            f = open(path, 'a', encoding='utf8')
             f.write(f'Дата и время записи функции: {datetime.now().strftime("%d %B %Y  time %H:%M:%S")}\n')
-            f.write(f'Имя функции: {get_path.__name__}\n')
+            f.write(f'Имя функции: {get_path_vk.__name__}\n')
             f.write(f'Аргументы: {args}, {kwargs}\n')
-            result = get_path(*args, **kwargs)
+            result = get_path_vk(*args, **kwargs)
             f.write(f'Результат: {result}\n')
             f.write(f'____________________\n')
             return result
@@ -19,10 +20,12 @@ def my_decorator(old_function):
     return decor
 
 @my_decorator(path)
-def get_path():
+def get_path_vk(url):
+    res = requests.get(url)
     with open(path, 'w', encoding='utf-8') as f:
         for i in f:
             yield hashlib.md5(i.encode()).hexdigest()
+    return res.text
 
-
-old_function = get_path()
+res = get_path_vk('https://cloud-api.yandex.net/v1/disk/resources/')
+print(res)
